89b97192b301c8ce0c470aca67f0aa28ceded04d
Romaisaa <romaisaa_elsaidy@stud.cu.edu.eg>
2022-05-24 15:42:25 +0200

    create available times getter

--- backend/Routes/roomRoute.js
+++ backend/Routes/roomRoute.js
@@ -3,7 +3,10 @@ const roomController = require("./../Controllers/roomController");
 
 const router = express.Router();
 
-router.route("/").post(roomController.createRoom);
+router
+  .route("/")
+  .post(roomController.createRoom)
+  .get(roomController.getAvailableRooms);
 router
   .route("/:id")
   .get(roomController.getRoom)
