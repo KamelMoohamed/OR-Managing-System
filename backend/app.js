2cca58d54e42843ff90c3c2d0642305cd63c7f6f
Romaisaa <romaisaa_elsaidy@stud.cu.edu.eg>
2022-05-28 14:54:59 +0200

    deleting patient model

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,6 @@ const path = require("path");
 const operationRoute = require("./Routes/operationRoute");
 const userRoute = require("./Routes/userRoute");
 const roomRoute = require("./Routes/roomRoute");
-const patientRoute = require("./Routes/patientRoute");
 const equipmentRoute = require("./Routes/equipmentRoute");
 const supplyRoute = require("./Routes/supplyRoute");
 const globalErrorHandler = require("./Controllers/errorController");
@@ -15,7 +14,6 @@ app.use(express.json());
 app.use("/api/v1/operation", operationRoute);
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/room", roomRoute);
-app.use("/api/v1/patient", patientRoute);
 app.use("/api/v1/equipment", equipmentRoute);
 app.use("/api/v1/supply", supplyRoute);
 
